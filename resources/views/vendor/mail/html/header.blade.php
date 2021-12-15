b35bc7aa305021f6a8b863f094e68fc8c91b319b
cod3mau5 <code.bit.mau@gmail.com>
2021-12-15 03:42:55 -0700

    sendMail

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -1,7 +1,7 @@
 <tr>
 <td class="header">
 <a href="{{ $url }}" style="display: inline-flex;">
-    <img src="{{ asset('assets/images/cabo_driver.webp') }}" class="w-100" style="width:100%;height:auto">
+    <img src="{{ asset('/assets/images/cabo_driver.webp') }}" class="w-100" style="width:100%;height:auto">
 </a>
 </td>
 </tr>
