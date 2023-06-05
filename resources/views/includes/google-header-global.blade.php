409825de341006b5a428a08e2fe31d5362088da7
cod3mau5 <code.bit.mau@gmail.com>
2023-06-04 21:00:13 -0700

    google analitics tag

--- resources/views/includes/google-header-global.blade.php
+++ resources/views/includes/google-header-global.blade.php
@@ -7,3 +7,13 @@
 
   gtag('config', 'AW-11080367620');
 </script>
+
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-NPCNZ8GPJ6"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-NPCNZ8GPJ6');
+</script>
