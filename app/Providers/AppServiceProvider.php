b8a0c5641866aa2a0c45947c08206d1eb9c1f7ab
cod3mau5 <code.bit.mau@gmail.com>
2023-06-06 19:27:57 -0700

    modification to use production env without errors

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,7 +24,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         if (env('APP_ENV') == 'production') {
-            \Illuminate\Support\Facades\URL::forceSchema('https');
+            # \Illuminate\Support\Facades\URL::forceSchema('https');
         }
     }
 }
