d4da20b8ac6d09382f72d1ebe1c4f1255168922b
cod3mau5 <code.bit.mau@gmail.com>
2023-06-06 20:36:33 -0700

    resolving conflict with laravel-cors in kernel.php

--- resources/views/pages/test/myEnv.blade.php
+++ resources/views/pages/test/myEnv.blade.php
@@ -5,8 +5,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>My Environment</title>
+    <style>
+        body *{
+            text-align: center;
+        }
+    </style>
 </head>
 <body>
     <h1>My environment is: {{env('APP_ENV')}}</h1>
+    <h2>Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})</h2>
 </body>
 </html>
