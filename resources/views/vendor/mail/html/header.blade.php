ad4dad6265b83b7e80a588f2b0a81b83dab48709
cod3mau5 <code.bit.mau@gmail.com>
2023-06-10 19:52:23 -0700

    table of showReservations changes

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -1,7 +1,7 @@
 <tr>
 <td class="header">
-<a href="{{ $url }}" style="display: inline-flex;">
-    <img src="https://www.cabodrivers.com/img/header_logo.png?" class="w-100" style="width:100%;height:auto">
+<a href="{{ $url }}" style="display: inline-flex;width:570px">
+    <img src="https://www.cabodrivers.com/img/header_logo.png?" class="w-100" style="width:66%;height:auto;margin:20px auto 0 auto;">
 </a>
 </td>
 </tr>
