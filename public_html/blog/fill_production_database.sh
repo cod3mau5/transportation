118f9e2c8ce19cadaac7264cd994cd36031322a1
cod3mau5 <code.bit.mau@gmail.com>
2023-06-21 12:35:22 -0700

    Añadido dump_2023_06_21_123521.sql volcado de la base de datos

---
+++ public_html/blog/fill_production_database.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Asegurarse de que el repositorio está actualizado
+git fetch
+git pull
+
+# Variables
+DB_USER="u606769855_cabodrivers_bl"
+DB_PASSWORD="Wifislaxutf-8"
+DB_NAME="u606769855_cabodrivers_bl"
+DUMP_DIR="sql_dumps/"
+
+# Obtener el nombre del archivo de volcado más reciente
+DUMP_FILE=$(ls -t $DUMP_DIR/*.sql | head -n 1)
+
+# Importar el volcado a la base de datos local
+mysql -u $DB_USER -p$DB_PASSWORD $DB_NAME < $DUMP_FILE
+
+# Reemplazar el nombre del dominio
+wp --allow-root search-replace 'http://cabodrivers.local/blog' 'https://cabodrivers.com/blog' --skip-columns=guid
