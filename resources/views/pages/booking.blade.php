a248c5871a68aaa90e94149f0c98f9b5c551c2f1
cod3mau5 <code.bit.mau@gmail.com>
2023-06-22 13:02:53 -0700

    link to blog on booking

--- resources/views/pages/booking.blade.php
+++ resources/views/pages/booking.blade.php
@@ -147,7 +147,7 @@
                         <a class="nav-link text-white"  href="{{route('book-now')}}">@{{ text.header.book_now }}</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link text-white"  href="{{route('book-now')}}">@{{ text.header.things_to_do }}</a>
+                        <a class="nav-link text-white"  href="/blog">@{{ text.header.things_to_do }}</a>
                     </li>
                     </ul>
 
