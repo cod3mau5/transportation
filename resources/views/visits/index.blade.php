826739d5b957fb78de9355bc25906b5d7826d91e
cod3mau5 <code.bit.mau@gmail.com>
2023-07-10 18:31:09 -0700

    starting web sockets development

--- resources/views/visits/index.blade.php
+++ resources/views/visits/index.blade.php
@@ -11,18 +11,10 @@
 
     <div class="box box-primary">
         <div class="box-header with-border">
-            <h3 class="box-title"><strong>VISITAS DE LA PAGINA</strong></h3>
-            {{-- <button type="button" class="btn btn-primary pull-right" id="abrir_modal"
-                data-toggle="collapse" href="#agregar" aria-expanded="false"
-                aria-controls="agregar"
-            >
-                Nuevo
-            </button> --}}
+            <h3 class="box-title"><strong>TODAS LAS VISITAS</strong></h3>
         </div>
         <div class="box-body">
-            {{-- <div class="collapse" id="agregar">
-                @include('zonas.form')
-            </div> --}}
+
             <div class="row">
                 <div class="col-md-12">
                     <table class="table table-bordered" id="datatable">
@@ -59,26 +51,6 @@
     <script>
         $(document).ready(function() {
 
-            // $.ajaxSetup({
-            //     headers: { 'X-CSRF-TOKEN': $('meta[name="_token"]').attr('content') }
-            // });
-
-            // var table = $('#datatable').DataTable({
-            //     pageLength: 25,
-            //     processing: true,
-            //     serverSide: true,
-            //     ajax: '{!! route('zonas.data') !!}',
-            //     columns: [
-            //         { data: 'nombre', name: 'nombre' },
-            //         { data: 'action',  name: 'action', orderable: false, searchable: false }
-            //     ],
-            //     rowId: 'id',
-            //     createdRow: function( row, data, dataIndex ) {
-            //         $( row ).find('td:eq(1)').css('text-align', 'right');
-            //     }
-            // });
-
-            // $('.form-horizontal').validate();
 
         });
     </script>
