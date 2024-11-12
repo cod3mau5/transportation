07c33b5b21947b3edd4a2ba178ebaaf5ea52de8c
cod3mau5 <admin@codemau5.com>
2024-11-11 22:04:46 -0700

    wirdpress integration to larvel

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -26,7 +26,7 @@ class HomeController extends Controller
     {
         return view('administracion');
     }
-    public function version()
+    public function env()
     {
         return view('pages.test.myEnv');
     }
